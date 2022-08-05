ffc6a0f3fad4d2295ac2be2fa4f688faa27239fd
alpheratz0 <alpheratz99@protonmail.com>
2022-08-05 18:07:53 -0300

    add cowthink message

--- xggen.c
+++ xggen.c
@@ -13,6 +13,16 @@
 	this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 	Place, Suite 330, Boston, MA 02111-1307 USA
 
+	 _________________
+	( I am inevitable )
+	 -----------------
+	  o
+	   o
+
+	      ■
+	        ■
+	    ■ ■ ■
+
 */
 
 #include <stdio.h>
